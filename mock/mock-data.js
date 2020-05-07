4ed227fc7552c578f609a9de218ba441c1b56888
lei <747942173@qq.com>
2020-05-07 17:02:54 +0800

    添加模拟数据

---
+++ mock/mock-data.js
@@ -0,0 +1,131 @@
+export default {
+	bids:[
+		{"id":"0","name":"全部"},
+		{"id":"1","name":"非遗"},
+		{"id":"2","name":"家具"},
+		{"id":"3","name":"茶叶"},
+		{"id":"4","name":"健康"},
+		{"id":"5","name":"旅游"},
+		{"id":"6","name":"传承"},
+		{"id":"7","name":"丝织"},
+	],
+	banner:[{
+				id: 0,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+			}, {
+				id: 1,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big37006.jpg',
+			}, {
+				id: 2,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+			}, {
+				id: 3,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+			}, {
+				id: 4,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+			}, {
+				id: 5,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+			}, {
+				id: 6,
+				type: 'image',
+				url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+			}],
+	subBids: [{
+		cuIcon: 'cardboardfill',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg',
+		color: 'red',
+		badge: 120,
+		name: 'VR'
+	}, {
+		cuIcon: 'recordfill',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg',
+		color: 'orange',
+		badge: 1,
+		name: '录像'
+	}, {
+		cuIcon: 'picfill',
+		color: 'yellow',
+		badge: 0,
+		name: '图像',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg',
+	}, {
+		cuIcon: 'noticefill',
+		color: 'olive',
+		badge: 22,
+		name: '通知',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg',
+	}, {
+		cuIcon: 'upstagefill',
+		color: 'cyan',
+		badge: 0,
+		name: '排行榜',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg',
+	}, {
+		cuIcon: 'clothesfill',
+		color: 'blue',
+		badge: 0,
+		name: '皮肤',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+	}, {
+		cuIcon: 'discoverfill',
+		color: 'purple',
+		badge: 0,
+		name: '发现',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+	}, {
+		cuIcon: 'questionfill',
+		color: 'mauve',
+		badge: 0,
+		name: '帮助',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+	}, {
+		cuIcon: 'commandfill',
+		color: 'purple',
+		badge: 0,
+		name: '问答',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+	}, {
+		cuIcon: 'brandfill',
+		color: 'mauve',
+		badge: 0,
+		name: '版权',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+	}],
+	hotGoods:[{
+		id: 0,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+	}, {
+		id: 1,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big37006.jpg',
+	}, {
+		id: 2,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+	}, {
+		id: 3,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+	}, {
+		id: 4,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+	}, {
+		id: 5,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+	}, {
+		id: 6,
+		type: 'image',
+		url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+	}],
+}
--- pages/tabbar/home/home.vue
+++ pages/tabbar/home/home.vue
@@ -1,25 +1,208 @@
 <template>
 	<view class="content">
-		页面 - 1
+		<view class="top-search align-center justify-center flex">
+			<view class=" top-search icon" :class="'cuIcon-search'"></view>
+			<view class="top-search text">新品上市春茶</view>
+		</view>
+		<scroll-view scroll-x class="bg-white nav" scroll-with-animation :scroll-left="scrollLeft">
+			<view class="cu-item" :class="index==TabCur?'cur':''" v-for="(item,index) in bids" :key="index" @tap="tabSelect" :data-id="item.id">
+				{{item.name}}
+			</view>
+		</scroll-view>
+		<swiper class="screen-swiper" :class="dotStyle?'square-dot':'round-dot'" :indicator-dots="true" :circular="true"
+				:autoplay="true" interval="5000" duration="500" indicator-color="#aaaaaa" indicator-active-color="#ffffff">
+			<swiper-item v-for="(item,index) in swiperList" :key="index" :class="cardCur==index?'cur':''">
+				<image :src="item.url" mode="aspectFill" v-if="item.type=='image'"></image>
+			</swiper-item>
+		</swiper>
+        <view class="cu-list grid" :class="['col-' + gridCol,gridBorder?'':'no-border']">
+            <view class="cu-item align-center justify-center" v-for="(item,index) in subBids" :key="index" v-if="index<gridCol*2">
+				<image class="class-icon" :src="item.url" mode="aspectFill"></image>
+                <text>{{item.name}}</text>
+            </view>
+        </view>
+		<view class="cu-list grid" :class="['col-2' ,gridBorder?'':'no-border']">
+		    <view class="flex align-center justify-start" v-for="(item,index) in subBids" :key="index" v-if="index<2*2">
+				<view class="flex flex-direction justify-start align-start">
+					<text class="hot-class-name">{{item.name}}</text>
+					<view class="flex align-center justify-start">
+						<view class="price-symbol">￥</view>
+						<view class="price-value">500</view>
+					</view>
+
+					<view class="pay-count"> 208人付款</view>
+					<view class="flex sell-state align-center justify-center" >
+						3日内有成交
+					</view>
+				</view>
+				<image class="hot-class-cover" :src="item.url" mode="aspectFill"></image>
+		    </view>
+		</view>
+        <view class="cu-list grid" :class="['col-2' ,gridBorder?'':'no-border']">
+		    <view class="cu-item" v-for="(item,index) in hotGoods" :key="index" v-if="index<2*3">
+		        <view class="flex justify-center align-center">
+                    <view class="price-symbol">￥</view>
+                    <view class="price-value">500</view>
+                    <view class="pay-count"> 208人付款</view>
+                </view>
+                <view class="sell-state align-center justify-center" >
+                    3日内有成交
+                </view>
+                <image class="ware-cover" :src="item.url" mode="aspectFill" v-if="item.type=='image'"></image>
+                <view class="ware-name">
+                    2020年新春热卖大红袍2.0金罐10罐装 新春热卖大红袍
+                </view>
+                <view  :class="['cuIcon-' + item.cuIcon,'text-' + item.color]">
+                </view>
+		    </view>
+		</view>
+
 	</view>
 </template>
 
 <script>
+	import mock from '@/mock/mock-data.js';
 export default {
+	onLoad() {
+		
+	},
+	onShow() {
+		this.bids = mock['bids'];
+		this.swiperList = mock['banner'];
+		this.subBids = mock.subBids;
+		this.hotGoods = mock.hotGoods;
+	},
 	data() {
 		return {
-			title: 'Hello'
+			cardCur: 0,
+			bids:[],
+			swiperList: [],//轮播图
+            subBids:[],
+            hotGoods:[],
+			dotStyle: true,
+			direction: '',
+			TabCur: 0,
+			scrollLeft: 0,
+            gridCol: 4,
+            gridBorder: false,
 		};
 	},
 	onLoad() {},
-	methods: {}
+	methods: {
+		tabSelect(e) {
+			this.TabCur = e.currentTarget.dataset.id;
+			this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60
+		}
+	}
 };
 </script>
 
-<style>
-.content {
-	text-align: center;
-	height: 400upx;
-	margin-top: 200upx;
-}
+<style lang="scss">
+	page {
+		background-color: #ffffff;
+		min-height: 100%;
+	}
+	.top-search {
+		width: 710rpx;
+		min-height: 64rpx;
+		background-color: #f2f2f2;
+		border-radius: 8rpx;
+		margin-left: 20rpx;
+		margin-top: 20rpx;
+		align-items: center;
+	}
+	.top-search.icon {
+		width: 25rpx;
+		height: 24rpx;
+		color: #999999;
+	}
+	.top-search.text {
+		font-family: PingFang-SC-Medium;
+		font-size: 24rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		letter-spacing: 0rpx;
+		width: 160rpx;
+		color: #999999;
+	}
+
+	.class-icon {
+		width: 109rpx;
+		height: 109rpx;
+	}
+
+	.hot-class-cover{
+		width: 200rpx;
+		height: 200rpx;
+	}
+	.hot-class-name {
+		font-family: PingFang-SC-Bold;
+		font-size: 32rpx;
+		font-weight: normal;
+		font-stretch: normal;
+		line-height: 58rpx;
+		letter-spacing: 0rpx;
+		color: #333333;
+	}
+
+    .price-symbol {
+        font-family: PingFang-SC-Bold;
+        font-size: 20rpx;
+        font-weight: normal;
+        font-stretch: normal;
+        line-height: 36rpx;
+        letter-spacing: 0rpx;
+        color: #333333;
+    }
+
+    .price-value{
+              font-family: PingFang-SC-Bold;
+              font-size: 28rpx;
+              font-weight: normal;
+              letter-spacing: 0rpx;
+              color: #333333;
+          }
+    .pay-count {
+        font-family: PingFang-SC-Bold;
+        font-size: 20rpx;
+        font-weight: normal;
+        font-stretch: normal;
+        line-height: 36rpx;
+        letter-spacing: 0rpx;
+        color: #999999;
+        flex-grow: 1;
+    }
+    .ware-cover {
+        width: 254rpx;
+        height: 203rpx;
+		margin-top: 20rpx;
+    }
+    .ware-name{
+        width: 243rpx;
+        font-family: PingFang-SC-Bold;
+        font-size: 22rpx;
+        font-weight: normal;
+        font-stretch: normal;
+        line-height: 36rpx;
+        letter-spacing: 0rpx;
+        color: #333333;
+    }
+    .sell-state {
+		display: flex;
+        width: 150rpx;
+        height: 36rpx;
+        background-color: #e60012;
+        border-radius: 6rpx;
+		margin-top: 10rpx;
+		padding-top: 3rpx;
+		padding-bottom: 3rpx;
+        font-family: PingFang-SC-Bold;
+        font-size: 12rpx;
+        font-weight: normal;
+        font-stretch: normal;
+        letter-spacing: 0rpx;
+        text-align: center;
+        color: #ffffff;
+
+    }
 </style>
